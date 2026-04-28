afb1f86860cac28a88318bfbb808adce0a2adeba
h4wks123 <ivannebayer@gmail.com>
2026-04-28 11:47:11 +0800

    feat: backend init, git ignore integration, and login changes

--- frontend/app/routes/entries/login.tsx
+++ frontend/app/routes/entries/login.tsx
@@ -39,9 +39,9 @@ export default function Login() {
         })}
         className="w-[min(100%,30rem)] relative left-1/2 top-1/2 -translate-1/2 space-y-8 bg-background text-default border border-secondary p-12 rounded-2xl z-50"
       >
-        <div className="flex flex-col justify-center mb-4">
+        <div className="flex flex-col justify-center gap-4">
           <div className="flex flex-col gap-1 mb-6">
-            <div className="flex gap-3 mx-auto justify-center items-center">
+            <div className="flex gap-4 mx-auto justify-center items-center">
               <div className="flex justify-center items-center bg-background border-2 border-primary/50 rounded-lg p-2">
                 <MessageSquare className="size-6 text-primary" />
               </div>
@@ -58,10 +58,10 @@ export default function Login() {
             control={form.control}
             name="email"
             render={({ field }) => (
-              <FormItem className="flex flex-col gap-3 mb-3">
+              <FormItem className="flex flex-col gap-4">
                 <FormLabel className="text-muted text-md">IDENTITY</FormLabel>
                 <FormControl>
-                  <InputGroup className="h-12 flex rounded-lg border-2 border-secondary">
+                  <InputGroup className="flex rounded-lg border-2 border-secondary">
                     <InputGroupInput
                       type="email"
                       autoComplete="email"
@@ -74,9 +74,7 @@ export default function Login() {
                     </InputGroupAddon>
                   </InputGroup>
                 </FormControl>
-                <div className="sm:h-4">
-                  <FormMessage className="text-error" />
-                </div>
+                <FormMessage className="text-error" />
               </FormItem>
             )}
           />
@@ -84,10 +82,10 @@ export default function Login() {
             control={form.control}
             name="password"
             render={({ field }) => (
-              <FormItem className="flex flex-col gap-3 mb-3">
+              <FormItem className="flex flex-col gap-4">
                 <FormLabel className="text-muted text-md">PASSCODE</FormLabel>
                 <FormControl>
-                  <InputGroup className="h-12 flex rounded-lg border-2 border-secondary">
+                  <InputGroup className="flex rounded-lg border-2 border-secondary">
                     <InputGroupInput
                       type="password"
                       autoComplete="password"
@@ -100,19 +98,22 @@ export default function Login() {
                     </InputGroupAddon>
                   </InputGroup>
                 </FormControl>
-                <div className="sm:h-4">
-                  <FormMessage className="text-error" />
-                </div>
+                <FormMessage className="text-error" />
               </FormItem>
             )}
           />
           <button
             type="submit"
-            className="h-12 w-full mb-6 flex justify-center items-center rounded-lg gap-1 border-2 border-accent hover:border-primary cursor-pointer"
+            className="h-12 w-full flex justify-center items-center rounded-lg gap-1 border-2 border-accent hover:border-primary cursor-pointer"
           >
             <h6 className="text-default text-md">INITIATE SESSION</h6>
             <ArrowRight className="size-4" />
           </button>
+          <div className="w-full gap-3 flex justify-center items-center">
+            <div className="bg-muted h-0.5 w-full" />
+            <p className="text-nowrap text-muted text-md">OR CONNECT VIA</p>
+            <div className="bg-muted h-0.5 w-full" />
+          </div>
           <div className="flex gap-2 mx-auto">
             <p className="text-muted text-md">New to chimer? </p>
             <Link to="/register" className="text-primary hover:underline">
