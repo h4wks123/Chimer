26b6efbf2627202e4ee9c32e03d196aa4520bc61
ivanne bayer <ivanne.bayer@tekkio.com>
2026-07-10 00:53:06 +0800

    fix: docker compose now includes psql db logic

--- backend/app/config/psql-db.ts
+++ backend/app/config/psql-db.ts
@@ -2,9 +2,9 @@ import { Pool } from "pg";
 import "dotenv/config";
 
 export const pool = new Pool({
-  host: process.env.DB_HOST,
-  port: Number(process.env.DB_PORT),
-  database: process.env.DB_NAME,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWD,
+  host: process.env.DATABASE_HOST,
+  port: Number(process.env.DATABASE_PORT),
+  database: process.env.DATABASE_NAME,
+  user: process.env.DATABASE_USERNAME,
+  password: process.env.DATABASE_PASSWORD,
 });
