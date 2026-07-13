778808304d1c678e314b60428617088daf7a33c9
ivanne bayer <ivanne.bayer@tekkio.com>
2026-07-13 15:18:38 +0800

    feat: improved error logging for user routes

--- backend/app/routes/users.ts
+++ backend/app/routes/users.ts
@@ -1,10 +1,21 @@
 import { Router } from "express";
 import { pool } from "../config/psql-db";
+import { pino } from "pino";
 
 const userRouter = Router();
+const logger = pino({
+  level: "info",
+  transport: {
+    target: "pino-pretty",
+    options: {
+      colorize: true,
+    },
+  },
+});
 
 userRouter.get("/", (req, res) => {
   const userId = req.query.userId;
+  logger.info(`Fetch all user information except for: ${userId}`);
 
   const sql = `SELECT * FROM users WHERE id != $1`;
 
@@ -12,9 +23,11 @@ userRouter.get("/", (req, res) => {
 
   pool.query(sql, values, (err, result) => {
     if (err) {
+      logger.error(`Failed to fetch information for user: ${userId}`);
       return res.status(500).send(err);
     }
 
+    logger.info(`Successfully fetched information from user: ${userId}`);
     res.status(200).json(result);
   });
 });
